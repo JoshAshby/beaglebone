e9f5c11849f5ba328c4b1828cd3c821b26fb7263
JoshAshby <joshuaashby@joshashby.com>
2013-07-18 00:08:34 -0600

    scan across leds? and set them as none for toggle

--- basics/leds/main.cpp
+++ basics/leds/main.cpp
@@ -10,6 +10,38 @@ using namespace std;
 
 const string LED_DIR = "/sys/class/leds/";
 
+int led_out(int led) {
+  string fileTemp = LED_DIR + "beaglebone:green:usr";
+  stringstream brightWhere;
+  brightWhere << fileTemp << led << "/brightness";
+  string filename = brightWhere.str();
+
+  fstream brightnessFile(filename.c_str(), fstream::out);
+  if(!brightnessFile.is_open()) {
+    cout << "Couldn't open the brightness file";
+    return -1;
+  };
+
+  brightnessFile << 0;
+
+  brightnessFile.close();
+
+  stringstream where;
+  where << fileTemp << led << "/trigger";
+  string filenameToggle = where.str();
+
+  fstream toggleFile(filenameToggle.c_str(), fstream::out);
+  if(!toggleFile.is_open()) {
+    cout << "Couldn't open the trigger file";
+    return -1;
+  };
+
+  toggleFile << "none";
+
+  toggleFile.close();
+  return 0;
+}
+
 int toggle_led(int led) {
   string fileTemp = LED_DIR + "beaglebone:green:usr";
   stringstream where;
@@ -22,29 +54,36 @@ int toggle_led(int led) {
     return -1;
   };
 
-  while(brightnessFile.good()) {
-    string val;
-    getline(brightnessFile, val);
-    int brightness = atoi(val.c_str());
-    cout << brightness << endl;
-    if(brightness >= 1) {
-      brightness = 0;
-    } else {
-      brightness = 1;
-    }
-    brightnessFile << brightness;
+  string val;
+  getline(brightnessFile, val);
+  int brightness = atoi(val.c_str());
+  cout << brightness << endl;
+  if(brightness >= 1) {
+    brightness = 0;
+  } else {
+    brightness = 1;
   };
+  brightnessFile << brightness;
 
   brightnessFile.close();
+
+  return 0;
 }
 
 int main() {
+  for(int i = 0; i <= 4; i++) {
+    led_out(i);
+  };
+
   cout << "Hi beagle!\n";
+
   while(true) {
-    toggle_led(0);
-    sleep(5);
-    toggle_led(0);
-    sleep(5);
-  }
+    for(int i = 0; i <= 4; i++) {
+      toggle_led(i);
+      sleep(1);
+      toggle_led(i);
+    };
+  };
+
   return 0;
 }
